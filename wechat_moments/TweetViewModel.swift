fa0c87f2521c7580ebc0e1dc8e995f5bf88362c6
Spadio9587 <hsx19961005@gmail.com>
2022-05-18 11:14:49 +0800

    feat: first version for getJson whci h has the callback to return to the main thread

--- wechat_moments/TweetViewModel.swift
+++ wechat_moments/TweetViewModel.swift
@@ -6,10 +6,32 @@
 //
 
 import Foundation
+import UIKit
 
 // task1:读取全部的数据，数据格式为数组
 // task2:筛选删除无效数据（error，unknown error, which does not contain a content and images）
-struct TweetViewModel {
+class TweetViewModel {
+    var tweet : [Tweet] = []
+    func getJson(callback: @escaping ([Tweet]) -> Void) {
+        let url = URL(string: "https://emagrorrim.github.io/mock-api/moments.json")!
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+               var tweet = try JSONDecoder().decode([Tweet].self, from: data )
+                tweet.removeAll(where: {$0.content == nil && $0.images == nil})
+                DispatchQueue.main.async {
+                    self.tweet = tweet
+                    callback(self.tweet)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+
     func getAllTweet() -> [Tweet]? {
         guard let data = momentAll.data(using: .utf8) else {
             return nil
