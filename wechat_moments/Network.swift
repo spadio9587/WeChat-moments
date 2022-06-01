7f5fe421c99d183c57ef50e3cf9cc88067256bf1
Spadio9587 <hsx19961005@gmail.com>
2022-06-01 14:58:34 +0800

    feat: add newWork level to deal with url data

--- wechat_moments/Network.swift
+++ wechat_moments/Network.swift
@@ -9,21 +9,40 @@ import Foundation
 
 class HttpNetWork {
     var userInfo: UserInfo?
-    func get(callback: @escaping () -> Void) {
-        let url = URL(string: "https://emagrorrim.github.io/mock-api/moments.json")
-        let task = URLSession.shared.dataTask(with: url!) {
-            data, _, _ in
+    func getTweet(urlString: String, callback:@escaping (([Tweet]?) -> Void)) {
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!) {data, _ , _ in
             guard data != nil else {
                 return
             }
-            let userInfo = self.decodeData(data: data!)
-            self.userInfo = userInfo
-            callback()
+            let tweet = self.decodeData(data: data!)
+            DispatchQueue.main.async {
+                callback(tweet)
+            }
         }
         task.resume()
     }
     
-    func decodeData(data: Data) -> UserInfo? {
+    func decodeData(data: Data) -> [Tweet]? {
+        let tweet = try? JSONDecoder().decode([Tweet].self, from: data)
+        return tweet
+    }
+    
+    func getUser(urlString: String, callback:@escaping (UserInfo?) -> Void) {
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!) {data, _, _ in
+            guard data != nil else {
+                return
+            }
+            let userInfo = self.decodeInfo(data: data!)
+            DispatchQueue.main.async {
+                callback(userInfo)
+            }
+        }
+        task.resume()
+    }
+    
+    func decodeInfo(data: Data) -> UserInfo? {
         let userInfo = try? JSONDecoder().decode(UserInfo.self, from: data)
         return userInfo
     }
---
+++ wechat_moments/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  wechat_moments
+//
+//  Created by Sixiao He on 2022/6/1.
+//
+
+import Foundation
